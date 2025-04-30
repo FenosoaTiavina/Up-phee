81d9bc4dac67a93fa67d74b93a8e9dbb5dc59f42
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-04-30 14:06:37 +0300

    t

--- build.zig
+++ build.zig
@@ -4,56 +4,15 @@ const builtin = @import("builtin");
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-
-    const uph_module = getModule(b, "uph", "uph");
-
-    const testbed_module = b.createModule(.{
-        .root_source_file = b.path("src/testbed/main.zig"),
-        .optimize = optimize,
-        .target = target,
-    });
-
-    const exe = b.addExecutable(.{
-        .name = "testbed",
-        .root_module = testbed_module,
-    });
-    exe.root_module.addImport("uph", uph_module);
 }
 
-pub const LibType = enum(i32) {
-    static,
-    dynamic, // requires DYLD_LIBRARY_PATH to point to the dylib path
-    exe_compiled,
-};
+fn setupRunStep(
+    b: *std.Build,
+    testbed: *std.Build.Step.Compile,
+) void {
+    const run_cmd = b.addRunArtifact(testbed);
+    run_cmd.step.dependOn(b.getInstallStep());
 
-pub fn getModule(b: *std.Build, comptime mod_name: []const u8, comptime _namespace: []const u8) *std.Build.Module {
-    return b.addModule(.{
-        .root_source_file = b.path("src/" ++ _namespace ++ "/uph.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
-        .name = mod_name,
-    });
-}
-
-/// prefix_path is used to add package paths. It should be the the same path used to include this build file
-pub fn linkArtifact(b: *std.Build, artifact: *std.Build.Step.Compile, lib_type: LibType, comptime name: []const u8, comptime namespace: []const u8) void {
-    const optimize = b.standardOptimizeOption(.{});
-    const target = b.standardTargetOptions(.{});
-    switch (lib_type) {
-        .static => {
-            const lib = b.addStaticLibrary(.{ .name = "uph", .root_source_file = "uph.zig", .optimize = optimize, .target = target });
-            b.installArtifact(lib);
-
-            artifact.linkLibrary(lib);
-        },
-        .dynamic => {
-            const lib = b.addSharedLibrary(.{ .name = "uph", .root_source_file = "uph.zig", .optimize = optimize, .target = target });
-            b.installArtifact(lib);
-
-            artifact.linkLibrary(lib);
-        },
-        else => {},
-    }
-
-    artifact.root_module.addImport("uph", getModule(name, namespace));
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_cmd.step);
 }
