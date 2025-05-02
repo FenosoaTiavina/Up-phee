b1a7fbe59b89d98f63c4bf5ace4a96aa514f98a8
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-05-02 17:00:56 +0300

    Files structure

--- src/uph.zig
+++ src/uph.zig
@@ -1,14 +1,12 @@
-pub const Renderer = @import("./renderer.zig");
-pub const Window = @import("./window.zig");
-pub const Types = @import("./types.zig");
-pub const Shader = @import("./shader.zig");
-pub const Events = @import("./event/event.zig");
-pub const clib = @import("./imports.zig");
-
 pub const ecs = @import("ecs");
 pub const zgui = @import("zgui");
 pub const zmath = @import("zmath");
 
 pub const Components = @import("./components/components.zig");
-pub const Maths = @import("utils/math.zig");
 pub const Config = @import("./config.zig");
+pub const Events = @import("./event/event.zig");
+pub const clib = @import("./imports.zig");
+pub const Renderer = @import("./renderer.zig");
+pub const Shader = @import("./shader.zig");
+pub const Types = @import("./types.zig");
+pub const Maths = @import("utils/math.zig");
